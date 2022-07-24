d82243ea2337746e29ea052cd191e88173bd137a
apuer <apuer110@gmail.com>
2022-07-24 13:09:10 +0800

    change args from docker envionment variables v1.1

--- setup.py
+++ setup.py
@@ -5,32 +5,45 @@
 # @File    : setup.py
 # @Project : zmq-consumer
 import asyncio
-import configparser
+import os
 import sys
 
 from loguru import logger
 
 from sample.zmq_consumer import write_influxdb
 
+# # # docker environment get args, annotation args from config file: influxdb.ini
+# # get influxdb config options
+# config = configparser.ConfigParser()
+# config.read("influxdb.ini", encoding="utf-8")
+# url = config['influxdb']['url']
+# token = config['influxdb']['token']
+# org = config['influxdb']['org']
+# bucket = config['influxdb']['bucket']
+#
+# # get zmq config options
+# host = config['zmq']['host']
+# port = config['zmq']['port']
+# topic = config['zmq']['topic'].encode('utf8')
+#
+# # get log file level
+# level = config['log_level']['level']
+
 # get influxdb config options
-config = configparser.ConfigParser()
-config.read("influxdb.ini", encoding="utf-8")
-url = config['influxdb']['url']
-token = config['influxdb']['token']
-org = config['influxdb']['org']
-bucket = config['influxdb']['bucket']
-
+url = os.environ['url']
+token = os.environ['org']
+org = os.environ['org']
+bucket = os.environ['bucket']
 # get zmq config options
-host = config['zmq']['host']
-port = config['zmq']['port']
-topic = config['zmq']['topic'].encode('utf8')
-
+host = os.environ['host']
+port = os.environ['port']
+topic = os.environ['topic']
 # get log file level
-level = config['log_level']['level']
+level = os.environ['level']
 
 # 定义console输出日志级别
 logger.remove()
-logger.add(sys.stdout, level="ERROR")
+logger.add(sys.stdout, level=level)
 # 日志, 每天创建一个新的log文件
 logger.add("./log/amon_Consumer_influxdb-{time:%Y-%m-%d}.log", rotation="00:00", enqueue=True, encoding="utf-8",
            level=level, retention='30 days')
